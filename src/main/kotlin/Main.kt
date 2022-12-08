04c2be8f560fad5db5dfad5d7afa8bfc4d3bb051
Ibrahim Alshubaily <ibrahim.alshubaily.sde@gmail.com>
2022-12-08 20:49:27 +0000

    dayTwoSolution first try

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,20 +1,10 @@
 import java.io.File
 
 fun main() {
-    dayOne()
+    dayTwo()
 }
 
-fun dayOne() {
-    val result = File("InputData/1.txt")
-        .readText()
-        .split("\n\n")
-        .map {
-            it.split("\n").map { v ->
-                v.toIntOrNull() ?: 0
-            }.sumOf { v -> v }
-        }
-    println(result.maxBy { it })
-
+//fun dayOne() {
 //    val sums = ArrayList<Int>()
 //    var currSum = 0
 //    File("InputData/1.txt").forEachLine {
@@ -26,4 +16,44 @@ fun dayOne() {
 //        }
 //    }
 //    println(sums.maxBy { it })
+//}
+
+fun dayTwo() {
+    val opponentChoiceMap = HashMap<String, String>()
+    opponentChoiceMap["A"] = "rock"
+    opponentChoiceMap["B"] = "paper"
+    opponentChoiceMap["C"] = "scissors"
+
+    val myChoiceMap = HashMap<String, String>()
+    myChoiceMap["X"] = "rock"
+    myChoiceMap["Y"] = "paper"
+    myChoiceMap["Z"] = "scissors"
+
+    val choiceScore = HashMap<String, Int>()
+    choiceScore["rock"] = 1
+    choiceScore["paper"] = 2
+    choiceScore["scissors"] = 3
+
+    fun isWin(opponentChoice: String, myChoice: String): Boolean {
+        return ((myChoice == "rock" && opponentChoice == "scissors") ||
+                (myChoice == "paper" && opponentChoice == "rock") ||
+                (myChoice == "scissors" && opponentChoice == "paper"))
+    }
+
+    fun isDraw(opponentChoice: String, myChoice: String) = opponentChoice == myChoice
+
+    var score = 0
+    File("InputData/2.txt").forEachLine {
+        val round = it.split(" ")
+
+        val opponentChoice = opponentChoiceMap[round[0]] ?: ""
+        val myChoice = myChoiceMap[round[1]] ?: ""
+        score += choiceScore[myChoice] ?: 0
+        if (isWin(opponentChoice, myChoice)) {
+            score += 6
+        } else if (isDraw(opponentChoice, myChoice)) {
+            score += 3
+        }
+    }
+    println(score)
 }
