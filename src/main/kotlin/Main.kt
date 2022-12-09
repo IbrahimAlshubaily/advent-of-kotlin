52c4d3b082e6b9731215011aba1f20bbad9ffada
Ibrahim Alshubaily <ibrahim.alshubaily.sde@gmail.com>
2022-12-09 09:28:39 +0000

    dayThreeSolution first try

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,68 @@
 import java.io.File
 
 fun main() {
-    dayTwo()
+    dayThree()
 }
+
+fun dayThree() {
+
+    val lowerBase = 'a'.code - 1
+    val upperBase = 'A'.code - 27
+    var out = 0
+    File("InputData/3.txt").forEachLine {
+
+        val compartmentLength = it.length / 2
+        val compartmentOne = it.substring(0, compartmentLength).toSet()
+        val compartmentTwo = it.substring(compartmentLength, it.length).toSet()
+
+        val sharedItem = compartmentOne.intersect(compartmentTwo).take(1)[0]
+        if (sharedItem.isLowerCase()) {
+            out += sharedItem.code - lowerBase
+        } else {
+            out += sharedItem.code - upperBase
+        }
+    }
+    println(out)//8153
+}
+//fun dayTwo() {
+//    val opponentChoiceMap = HashMap<String, String>()
+//    opponentChoiceMap["A"] = "rock"
+//    opponentChoiceMap["B"] = "paper"
+//    opponentChoiceMap["C"] = "scissors"
+//
+//    val myChoiceMap = HashMap<String, String>()
+//    myChoiceMap["X"] = "rock"
+//    myChoiceMap["Y"] = "paper"
+//    myChoiceMap["Z"] = "scissors"
+//
+//    val choiceScore = HashMap<String, Int>()
+//    choiceScore["rock"] = 1
+//    choiceScore["paper"] = 2
+//    choiceScore["scissors"] = 3
+//
+//    fun isWin(opponentChoice: String, myChoice: String): Boolean {
+//        return ((myChoice == "rock" && opponentChoice == "scissors") ||
+//                (myChoice == "paper" && opponentChoice == "rock") ||
+//                (myChoice == "scissors" && opponentChoice == "paper"))
+//    }
+//
+//    fun isDraw(opponentChoice: String, myChoice: String) = opponentChoice == myChoice
+//
+//    var score = 0
+//    File("InputData/2.txt").forEachLine {
+//        val round = it.split(" ")
+//
+//        val opponentChoice = opponentChoiceMap[round[0]] ?: ""
+//        val myChoice = myChoiceMap[round[1]] ?: ""
+//        score += choiceScore[myChoice] ?: 0
+//        if (isWin(opponentChoice, myChoice)) {
+//            score += 6
+//        } else if (isDraw(opponentChoice, myChoice)) {
+//            score += 3
+//        }
+//    }
+//    println(score)
+//}
 
 //fun dayOne() {
 //    val sums = ArrayList<Int>()
@@ -18,42 +78,6 @@ fun main() {
 //    println(sums.maxBy { it })
 //}
 
-fun dayTwo() {
-    val opponentChoiceMap = HashMap<String, String>()
-    opponentChoiceMap["A"] = "rock"
-    opponentChoiceMap["B"] = "paper"
-    opponentChoiceMap["C"] = "scissors"
 
-    val myChoiceMap = HashMap<String, String>()
-    myChoiceMap["X"] = "rock"
-    myChoiceMap["Y"] = "paper"
-    myChoiceMap["Z"] = "scissors"
 
-    val choiceScore = HashMap<String, Int>()
-    choiceScore["rock"] = 1
-    choiceScore["paper"] = 2
-    choiceScore["scissors"] = 3
 
-    fun isWin(opponentChoice: String, myChoice: String): Boolean {
-        return ((myChoice == "rock" && opponentChoice == "scissors") ||
-                (myChoice == "paper" && opponentChoice == "rock") ||
-                (myChoice == "scissors" && opponentChoice == "paper"))
-    }
-
-    fun isDraw(opponentChoice: String, myChoice: String) = opponentChoice == myChoice
-
-    var score = 0
-    File("InputData/2.txt").forEachLine {
-        val round = it.split(" ")
-
-        val opponentChoice = opponentChoiceMap[round[0]] ?: ""
-        val myChoice = myChoiceMap[round[1]] ?: ""
-        score += choiceScore[myChoice] ?: 0
-        if (isWin(opponentChoice, myChoice)) {
-            score += 6
-        } else if (isDraw(opponentChoice, myChoice)) {
-            score += 3
-        }
-    }
-    println(score)
-}
